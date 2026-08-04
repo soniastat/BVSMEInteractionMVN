63e094431ee13e41d9ef8d4ade622cf6f3057c28
soniastat <soniakhatun.du.29@gmail.com>
2026-08-04 09:39:37 -0600

    previous testing files upload

---
+++ testing_files1/testing_BVS_HRHS_SI_MVN_cov.R
@@ -0,0 +1,200 @@
+
+# Testing codes for Bayesian variable selection with hierarchical regularized
+# Horseshoe priors and Shared information across outcomes for multivariate
+# normal response (BVS-HRHS-SI-MVN)
+
+rm(list = ls())
+gc()
+
+###############################
+
+# Run all the functions from the package
+devtools::load_all()
+
+library(doParallel) # for parallelization
+library(foreach) # for parallelization
+library(LaplacesDemon) # IW
+
+library(doRNG) # used for instead of set.seed
+
+library(ggplot2) # plot
+
+#######################################################
+
+# real data
+options(echo=TRUE)
+options(stringsAsFactors = FALSE)
+
+
+library(tidyverse)
+library(readxl)
+
+
+load("exposome_data_analysis/exposome.RData")
+load("exposome_data_analysis/modifiers_covariates.rda")
+load("exposome_data_analysis/exposures.rda")
+
+names(covariates)
+
+
+names(phenotype)
+
+covariates_post <- codebook %>%
+  filter(domain=="Covariates" & period == "Postnatal") %>%
+  pull(variable_name) %>%
+  as.character()
+covariates_post <- covariates_post[covariates_post %in%
+                                     c("hs_c_height_None", "hs_c_weight_None")]
+cov_base_post <- covariates[,covariates_post]
+
+
+# Combine responses/covariates
+raw_Y <- cbind(phenotype[, 5:6], cov_base_post)
+Y <- scale(raw_Y)
+
+Z <- cbind(Organochlorines_Postnatal, Metals_Postnatal) # only postnatal
+
+J <- dim(X)[2]
+M <- dim(Z)[2]
+O <- dim(D)[2]
+K <- dim(Y)[2]
+n <- dim(Y)[1]
+
+# Modifier-environment interaction terms
+U <- matrix(0, n, J * M)
+col_idx <- 1
+for (j in 1:J) {
+  for (m in 1:M) {
+    U[, col_idx] <- X[, j] * Z[, m]
+    col_idx <- col_idx + 1
+  }
+}
+
+# modifiers = X, covariates = D
+
+W <- cbind(1, X, Z, U, D)
+n_all_par <- dim(W)[2]
+
+
+#######################################################
+# Fit the BVS-HRHS-SI-MVN-cov model
+
+start_time_BVS_HRHS_SI_MVN_cov <- Sys.time()
+set.seed(3567)
+res_all_par_BVS_HRHS_SI_MVN_cov <- fit_BVS_HRHS_SI_MVN_cov(
+  niter = 6000, burn_in = 1000, thin = 5,
+  n = n, K = K, Y = Y, W = W, n_all_par = n_all_par, J = J, M = M, O,
+  c = 2.5,
+  theta_init = matrix(0.5, nrow = n_all_par, ncol = K),
+  lambdasq_beta_init = rep(0.5, J),
+  tausq_beta_init = 1,
+  lambdasq_gamma_init = rep(0.5, M),
+  tausq_gamma_init = 1,
+  lambdasq_delta_init = rep(0.5, J*M),
+  tausq_delta_init = 1,
+  psi_beta_init = rep(0.5, J),
+  psi_gamma_init = rep(0.5, M),
+  psi_delta_init = rep(0.5, J*M),
+  xi_beta_init = 1, xi_gamma_init = 1,
+  xi_delta_init = 1,
+  Sigma_init = Sigma_init,
+  nu_0 = nu_0, Psi_0 = Psi_0,
+  omegasq_beta_lambdasq = 0.25, omegasq_beta_tausq = 0.25,
+  omegasq_gamma_lambdasq = 0.25, omegasq_gamma_tausq = 0.25,
+  omegasq_delta_lambdasq = 0.25, omegasq_delta_tausq = 0.25,
+  accept_lambdasq_beta_init = rep(1, times = J),
+  accept_tausq_beta_init = 1,
+  accept_lambdasq_gamma_init = rep(1, times = M),
+  accept_tausq_gamma_init = 1,
+  accept_lambdasq_delta_init = rep(1, times = J*M),
+  accept_tausq_delta_init = 1,
+  sigmasq_varphi = 10
+)
+end_time_BVS_HRHS_SI_MVN_cov <- Sys.time()
+end_time_BVS_HRHS_SI_MVN_cov - start_time_BVS_HRHS_SI_MVN_cov
+
+# save(res_all_par_BVS_HRHS_SI_MVN_cov, file = "exposome_data_analysis/res_n1301_O6_M17_J44_K5_6000ite_1000burn_5thin_BVSHRHSSIMVNcov_postexpo.rda")
+# load(file = "exposome_data_analysis/res_n1301_O6_M17_J44_K5_6000ite_1000burn_5thin_BVSHRHSSIMVNcov_postexpo.rda")
+
+
+###########################################################################################
+start_time_BVS_HRHS_SI_MVN_cov_chain2 <- Sys.time()
+set.seed(433578)
+res_all_par_BVS_HRHS_SI_MVN_cov_chain2 <- fit_BVS_HRHS_SI_MVN_cov(
+  niter = 6000, burn_in = 1000, thin = 5,
+  n = n, K = K, Y = Y, W = W, n_all_par = n_all_par, J = J, M = M, O,
+  c = 2.5,
+  theta_init = matrix(0.15, nrow = n_all_par, ncol = K),
+  lambdasq_beta_init = rep(0.15, J),
+  tausq_beta_init = 5,
+  lambdasq_gamma_init = rep(0.15, M),
+  tausq_gamma_init = 5,
+  lambdasq_delta_init = rep(0.15, J*M),
+  tausq_delta_init = 5,
+  psi_beta_init = rep(0.15, J),
+  psi_gamma_init = rep(0.15, M),
+  psi_delta_init = rep(0.15, J*M),
+  xi_beta_init = 5, xi_gamma_init = 5,
+  xi_delta_init = 5,
+  Sigma_init = Sigma_init,
+  nu_0 = nu_0, Psi_0 = Psi_0,
+  omegasq_beta_lambdasq = 0.25, omegasq_beta_tausq = 0.25,
+  omegasq_gamma_lambdasq = 0.25, omegasq_gamma_tausq = 0.25,
+  omegasq_delta_lambdasq = 0.25, omegasq_delta_tausq = 0.25,
+  accept_lambdasq_beta_init = rep(1, times = J),
+  accept_tausq_beta_init = 1,
+  accept_lambdasq_gamma_init = rep(1, times = M),
+  accept_tausq_gamma_init = 1,
+  accept_lambdasq_delta_init = rep(1, times = J*M),
+  accept_tausq_delta_init = 1,
+  sigmasq_varphi = 10
+)
+end_time_BVS_HRHS_SI_MVN_cov_chain2 <- Sys.time()
+end_time_chain2 - start_time_BVS_HRHS_SI_MVN_cov_chain2
+
+# save(res_all_par_BVS_HRHS_SI_MVN_cov_chain2, file = "exposome_data_analysis/res_n1301_O6_M17_J44_K5_6000ite_1000burn_5thin_BVSHRHSSIMVNcov_postexpo_chain2.rda")
+# load(file = "exposome_data_analysis/res_n1301_O6_M17_J44_K5_6000ite_1000burn_5thin_BVSHRHSSIMVNcov_postexpo_chain2.rda")
+
+
+#######################################################
+
+start_time_BVS_HRHS_SI_MVN_cov_chain3 <- Sys.time()
+set.seed(987656656)
+res_all_par_BVS_HRHS_SI_MVN_cov_chain3 <- fit_BVS_HRHS_SI_MVN_cov(
+  niter = 6000, burn_in = 1000, thin = 5,
+  n = n, K = K, Y = Y, W = W, n_all_par = n_all_par, J = J, M = M, O,
+  c = 2.5,
+  theta_init = matrix(2.5, nrow = n_all_par, ncol = K),
+  lambdasq_beta_init = rep(2.5, J),
+  tausq_beta_init = 0.1,
+  lambdasq_gamma_init = rep(2.5, M),
+  tausq_gamma_init = 0.1,
+  lambdasq_delta_init = rep(2.5, J*M),
+  tausq_delta_init = 0.1,
+  psi_beta_init = rep(2.5, J),
+  psi_gamma_init = rep(2.5, M),
+  psi_delta_init = rep(2.5, J*M),
+  xi_beta_init = 0.1, xi_gamma_init = 0.1,
+  xi_delta_init = 0.1,
+  Sigma_init = Sigma_init,
+  nu_0 = nu_0, Psi_0 = Psi_0,
+  omegasq_beta_lambdasq = 0.25, omegasq_beta_tausq = 0.25,
+  omegasq_gamma_lambdasq = 0.25, omegasq_gamma_tausq = 0.25,
+  omegasq_delta_lambdasq = 0.25, omegasq_delta_tausq = 0.25,
+  accept_lambdasq_beta_init = rep(1, times = J),
+  accept_tausq_beta_init = 1,
+  accept_lambdasq_gamma_init = rep(1, times = M),
+  accept_tausq_gamma_init = 1,
+  accept_lambdasq_delta_init = rep(1, times = J*M),
+  accept_tausq_delta_init = 1,
+  sigmasq_varphi = 10
+)
+end_time_BVS_HRHS_SI_MVN_cov_chain3 <- Sys.time()
+end_time_BVS_HRHS_SI_MVN_cov_chain3 - start_time_BVS_HRHS_SI_MVN_cov_chain3
+
+# save(res_all_par_BVS_HRHS_SI_MVN_cov_chain3, file = "exposome_data_analysis/res_n1301_O6_M17_J44_K5_6000ite_1000burn_5thin_BVSHRHSSIMVNcov_postexpo_chain3.rda")
+# load(file = "exposome_data_analysis/res_n1301_O6_M17_J44_K5_6000ite_1000burn_5thin_BVSHRHSSIMVNcov_postexpo_chain3.rda")
+
+#############################################################
+
+
