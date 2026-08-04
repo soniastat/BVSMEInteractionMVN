2133be177d20d06b23327e4e8bf42c8acca6da21
soniastat <soniakhatun.du.29@gmail.com>
2026-08-04 09:42:09 -0600

    upload all previous code under folders

---
+++ all_modify2_codes/alpine_run_one_sample_data_modify2_allvarsim.R
@@ -0,0 +1,84 @@
+
+
+# Simulated data for all var real
+
+sample_id <- as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+
+cat("Running sample:", sample_id, "\n")
+
+###################################################
+
+source("/projects/soniast@colostate.edu/alpine_sim_HHS_modify2.R")
+
+
+
+library(LaplacesDemon)
+
+
+
+############################
+# Load data
+############################
+
+load("/projects/soniast@colostate.edu/exposome.RData")
+load("/projects/soniast@colostate.edu/modifiers_covariates.rda")
+load("/projects/soniast@colostate.edu/exposures.rda")
+load("/projects/soniast@colostate.edu/cov_base_post.rda")
+
+
+
+raw_Y <- cbind(
+  phenotype[,5:6],
+  cov_base_post
+)
+
+Y0 <- scale(raw_Y)
+
+Z <- cbind(
+  Organochlorines_Postnatal,
+  Metals_Postnatal
+)
+
+
+Sigma <- cov(Y0)
+K <- dim(Y0)[2]
+n <- nrow(Y0)
+J <- ncol(X)
+M <- ncol(Z)
+O <- ncol(D)
+
+
+############################
+# Simulate one dataset all var simulated
+############################
+
+set.seed(23444 + sample_id)
+sim_data <- Sim_data_BVS_real_sim_var(
+  K = K,
+  n = n,
+  J = J,
+  M = M,
+  O = O,
+  Sigma = Sigma
+)
+
+
+
+
+dir.create("/projects/soniast@colostate.edu/results_sample_data_1301_alvarsim", recursive=TRUE, showWarnings=FALSE)
+
+outfile <- paste0("/projects/soniast@colostate.edu/results_sample_data_1301_alvarsim/sample_data_1301_alvarsim_", sample_id, ".rds")
+
+saveRDS(sim_data, file=outfile)
+
+cat("Finished sample", sample_id, "\n")
+
+
+
+
+
+
+
+
+
+
---
+++ all_modify3_codes/alpine_run_simulated_data_n1301_hhs_modify3.R
@@ -0,0 +1,144 @@
+
+# Testing codes
+
+sample_id <- as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+
+cat("Running sample:", sample_id, "\n")
+
+###################################################
+
+source("/projects/soniast@colostate.edu/alpine_sim_HHS_modify3.R")
+
+
+
+library(LaplacesDemon)
+library(Matrix)
+
+
+
+
+
+load("/projects/soniast@colostate.edu/exposome.RData")
+load("/projects/soniast@colostate.edu/modifiers_covariates.rda")
+load("/projects/soniast@colostate.edu/exposures.rda")
+load("/projects/soniast@colostate.edu/cov_base_post.rda")
+
+
+
+raw_Y <- cbind(
+  phenotype[,5:6],
+  cov_base_post
+)
+
+Y0 <- scale(raw_Y)
+
+Z <- cbind(
+  Organochlorines_Postnatal,
+  Metals_Postnatal
+)
+
+
+Sigma <- cov(Y0)
+K <- dim(Y0)[2]
+n <- nrow(Y0)
+J <- ncol(X)
+M <- ncol(Z)
+O <- ncol(D)
+
+############################
+# Simulate one dataset
+############################
+
+set.seed(23444 + sample_id)
+sim_data <- Sim_data_BVS_real_sim_var(
+  K = K,
+  n = n,
+  J = J,
+  M = M,
+  O = O,
+  Sigma = Sigma
+)
+
+
+
+Y <- sim_data$Y
+x <- sim_data$x
+z <- sim_data$z
+u <- sim_data$u
+d <- sim_data$d
+W <- sim_data$W
+
+WTW <- t(W) %*% W
+
+n_all_par <- ncol(W)
+nu_0 <- K + 2
+Psi_0 <- diag(K)
+Sigma_init <- rinvwishart(nu_0, Psi_0)
+
+
+
+
+
+############################
+# Fit HHS model
+############################
+
+set.seed(765878 + sample_id)
+
+theta_update_save1 <- tryCatch({
+
+  res <- fit_HHS_MVN_cov_modify3(
+    niter = 6000, burn_in = 1000, thin = 5,
+    n = n, K = K, Y = Y, W = W, WTW = WTW,
+    n_all_par = n_all_par, J = J, M = M, O = O,
+    sigmasq_alpha = 100,
+    theta_init = matrix(0.5, nrow = n_all_par, ncol = K),
+    lambdasq_beta_init = matrix(0.5, nrow = J, ncol = K),
+    tausq_beta_init = rep(1, J),
+    lambdasq_gamma_init = matrix(0.5, nrow = M, ncol = K),
+    tausq_gamma_init = rep(1, M),
+    lambdasq_delta_init = matrix(0.5, nrow = J*M, ncol = K),
+    tausq_delta_init = rep(1, J*M),
+    psi_beta_init = matrix(0.5, nrow = J, ncol = K),
+    psi_gamma_init = matrix(0.5, nrow = M, ncol = K),
+    psi_delta_init = matrix(0.5, nrow = J*M, ncol = K),
+    xi_beta_init = 1, xi_gamma_init = 1,
+    xi_delta_init = 1,
+    Sigma_init = Sigma_init,
+    nu_0 = nu_0, Psi_0 = Psi_0,
+    sigmasq_varphi = 10
+  )
+
+  res$theta_update
+
+}, error=function(e){
+  list(
+    error=TRUE,
+    message=e$message,
+    sample_id=sample_id
+  )
+
+})
+
+
+
+
+dir.create("/projects/soniast@colostate.edu/results_HHS_simdata_n1301_modify3", recursive=TRUE, showWarnings=FALSE)
+
+outfile <- paste0("/projects/soniast@colostate.edu/results_HHS_simdata_n1301_modify3/modify3_n1301_HHS_cov_sample_", sample_id, ".rds")
+
+saveRDS(theta_update_save1, file=outfile)
+
+cat("Finished HHS sample", sample_id, "\n")
+
+########################
+
+
+
+
+
+
+
+
+
+
